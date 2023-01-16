1b30f1f16895ecf17641755f1909de7e7c42c9fc
johnyww <108518940+johnyww@users.noreply.github.com>
2023-01-16 11:57:49 +0800

    v0.6

---
+++ auto.bat
@@ -0,0 +1,6 @@
+cd C:\Users\Iman\Documents\GitHub\johnyww.github.io\info
+python hehe5.py
+cd C:\Users\Iman\Documents\GitHub\johnyww.github.io\photo
+python hehe4.py
+pause
+echo "Press any key to continue"
